d14d171b7ce1252d3ffb6e622d1dd0634dfd38d4
Tiago Emanuel <tiago.emanuel.n@gmail.com>
2022-03-14 09:19:16 -0300

    req2: cria funcao com dias que a pessoa nunca visitou

--- src/track_orders.py
+++ src/track_orders.py
@@ -15,22 +15,36 @@ class TrackOrders:
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         # quero filtrar os clientes
-        filtra_cliente = [item[1] for item in self.list if item[0] == costumer]
+        filtra_cliente = [
+            indice[1] for indice in self.list if indice[0] == costumer
+        ]
         # nesse resultado quero apenas a chave, por isso essa mini gambiarra
         resultado = collections.Counter(filtra_cliente).most_common()[0][0]
         return resultado
 
     # mesma lógica de teoria dos conjuntos
     def get_never_ordered_per_costumer(self, costumer):
-        conjunto_pratos_cliente = {  # conjunto geral
-            item[1] for item in self.list if costumer not in item
+        # pratos de clientes
+        conjunto_pratos_cliente = {
+            indice[1] for indice in self.list if costumer not in indice
         }
-        conjunto_pratos = {item[1] for item in self.list if costumer in item}
+        # pratos gerais
+        conjunto_pratos = {
+            indice[1] for indice in self.list if costumer in indice
+        }
         # complementar de conjuntos
         return conjunto_pratos_cliente - conjunto_pratos
 
+    # repete a lógica de teoria dos conjuntos
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        conjunto_dias = {
+            indice[2] for indice in self.list if costumer not in indice
+        }
+        conjunto_dias_gerais = {
+            indice[2] for indice in self.list if costumer in indice
+        }
+        # complementar de conjuntos
+        return conjunto_dias - conjunto_dias_gerais
 
     def get_busiest_day(self):
         pass
