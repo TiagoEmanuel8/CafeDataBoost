84b46b47370bc6801f5d96c8673f6fd1c5d04ad7
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-12-13 10:28:32 -0300

    feat: refatorando funções

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,52 +1,31 @@
 import csv
 import collections
 
-
-# lendo o csv
 def leitura_csv(path_file):
     with open(path_file) as file:
         dados_csv = csv.reader(file)
-        # quero ler o csv e iterar em lista
         dados_lista = [indice for indice in dados_csv]
     return dados_lista
 
-
-# prato mais pedido por maria
 def pedidos_preferidos_maria(data):
     filtra_pratos = [indice[1] for indice in data if indice[0] == 'maria']
-    # counter vai me trazer as chaves e valores dos pratos de maria, num dict
     resposta = collections.Counter(filtra_pratos)
-    # esse key é semelhante ao object.key() do js
     return max(resposta, key=resposta.get)
 
-
-# quantas vezes arnaldo pediu hamburguer?
 def arnaldo_pedidos_hamburguer(data):
-    # mesmo raciocínio da função anterior
     filtra_prato = [item[1] for item in data if item[0] == 'arnaldo']
     return filtra_prato.count('hamburguer')
 
-
-# quais pratos joão nunca pediu?
-# para fazer esses requisito, fiz baseado na teoria de conjuntos
 def joao_nunca_pediu(data):
-    # filtra todos os pratos do joão
     pratos_joao = {item[1] for item in data if item[0] == 'joao'}
-    # filtra todos os pratos do csv
     pratos_gerais = {item[1] for item in data if item[1]}
-    # complementar de conjuntos
     return pratos_gerais - pratos_joao
 
-
-# quais dias joão nunca foi na lanchonete?
-# mesmo raciocínio da função anterior
 def joao_nao_frequentou(data):
     foi = {item[2] for item in data if item[0] == 'joao'}
     geral = {item[2] for item in data if item[1]}
     return geral - foi
 
-
-# escrevendo nos arquivos
 def analyze_log(path_to_file):
     dados_csv = leitura_csv(path_to_file)
     with open("data/mkt_campaign.txt", "w") as file:
--- src/track_orders.py
+++ src/track_orders.py
@@ -6,36 +6,27 @@ class TrackOrders:
         self.list = []
 
     def __len__(self):
-        # aponta para lista vazia - validação 2.1
         return len(self.list)
 
     def add_new_order(self, costumer, order, day):
-        # adiciono dados na lista
         self.list.append((costumer, order, day))
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        # quero filtrar os clientes
         filtra_cliente = [
             indice[1] for indice in self.list if indice[0] == costumer
         ]
-        # nesse resultado quero apenas a chave, por isso essa mini gambiarra
         resultado = collections.Counter(filtra_cliente).most_common()[0][0]
         return resultado
 
-    # mesma lógica de teoria dos conjuntos
     def get_never_ordered_per_costumer(self, costumer):
-        # pratos de clientes
         conjunto_pratos_cliente = {
             indice[1] for indice in self.list if costumer not in indice
         }
-        # pratos gerais
         conjunto_pratos = {
             indice[1] for indice in self.list if costumer in indice
         }
-        # complementar de conjuntos
         return conjunto_pratos_cliente - conjunto_pratos
 
-    # repete a lógica de teoria dos conjuntos
     def get_days_never_visited_per_costumer(self, costumer):
         conjunto_dias = {
             indice[2] for indice in self.list if costumer not in indice
@@ -43,7 +34,6 @@ class TrackOrders:
         conjunto_dias_gerais = {
             indice[2] for indice in self.list if costumer in indice
         }
-        # complementar de conjuntos
         return conjunto_dias - conjunto_dias_gerais
 
     def get_busiest_day(self):
